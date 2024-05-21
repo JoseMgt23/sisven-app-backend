987a4cd290ad469e08bc52c4ff7c6c83cf4a587f
Jose Manuel Gonzalez Trochez <joseclasef3ae@gmail.com>
2024-05-21 13:45:15 -0500

    Ruta de Productos

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\api\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/products', [ProductController::class,'store'])->name('products.store');
+Route::get('/products' , [ProductController::class,'index'])->name('products');
+Route::delete('/products/{product}', [ProductController::class,'destroy'])->name('products.destroy');
+Route::get('/products/{product}',[ProductController::class,'show'])->name('products.show');
+Route::put('/products/{product}',[ProductController::class,'update'])->name('products.update');
