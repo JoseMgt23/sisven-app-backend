1a9804d0955fc454fe56dbc297497a9b3264299e
Jose Manuel Gonzalez Trochez <joseclasef3ae@gmail.com>
2024-05-21 11:08:04 -0500

    Modelo Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $table = "products";
+    protected $primaryKey = "id_product";
+    public $timestamps = false;
+    protected $fillable = [
+        'name',
+        'price',
+        'stock',
+        'id_categorias',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Categoria::class);
+    }
 }
