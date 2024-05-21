e6fcbe59433635cb88c6ece772ac3e95abe15afe
Jose Manuel Gonzalez Trochez <joseclasef3ae@gmail.com>
2024-05-21 14:40:28 -0500

    Ruta api de Categoria

--- app/Http/Controllers/api/ProductController.php
+++ app/Http/Controllers/api/ProductController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Http\Request;    
 use Illuminate\Support\Facades\DB;
 use App\Models\Product;
 
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php 
 
 use App\Http\Controllers\api\ProductController;
+use App\Http\Controllers\api\CategoriaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,13 @@ Route::delete('/products/{product}', [ProductController::class,'destroy'])->name
 Route::get('/products/{product}',[ProductController::class,'show'])->name('products.show');
 Route::put('/products/{product}',[ProductController::class,'update'])->name('products.update');
 
+
+
+Route::post('/categorias', [CategoriaController::class, 'store'])->name('categorias.store');
+Route::get('/categorias', [CategoriaController::class, 'index'])->name('categorias.index');
+Route::get('/categorias/{categoria}', [CategoriaController::class, 'show'])->name('categorias.show');
+Route::put('/categorias/{categoria}', [CategoriaController::class, 'update'])->name('categorias.update');
+Route::delete('/categorias/{categoria}', [CategoriaController::class, 'destroy'])->name('categorias.destroy');
 
 
 
