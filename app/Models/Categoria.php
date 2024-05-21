de35d597ef8eab0f73c16b5123042790d4f9ba3e
Jose Manuel Gonzalez Trochez <joseclasef3ae@gmail.com>
2024-05-21 11:06:50 -0500

    modelo Categoria

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Categoria extends Model
 {
     use HasFactory;
+
+    protected $table = "categorias";
+    protected $primaryKey = "id_categorias";
+    public $timestamps = false;
+
+    public function products()
+{
+    return $this->hasMany(Product::class);
+}
 }
