6c0e68cf2d8e3aff87f7d5bcfcef93ecbd228555
Jose Manuel Gonzalez Trochez <joseclasef3ae@gmail.com>
2024-05-21 11:07:17 -0500

    Modelo Customer

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
+    protected $table = "customers";
+    protected $primaryKey = "id_customers";
+    public $timestamps = false;
+    protected $fillable = [
+        'document_number',
+        'first_name',
+        'last_name',
+        'address',
+        'birthday',
+        'phone_number',
+        'email',
+    ];
 }
